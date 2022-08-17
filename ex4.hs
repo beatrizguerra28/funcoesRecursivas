5ae5e37e7497841990a47322b013fe68ea16de23
beatrizguerra28 <83786167+beatrizguerra28@users.noreply.github.com>
2022-08-17 08:21:18 -0300

    Update ex4.hs

--- ex4.hs
+++ ex4.hs
@@ -1,6 +1,6 @@
 euclides  m n
     | m == n = n
-    | otherwise = euclides(max m n - min m n) (min m n)
+    | otherwise = euclides(maximo- minimo) minimo 
     where
         maximo = max m n
         minimo = min m n
