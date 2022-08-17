11e0f995bf1367c9f4891f31df1d3333558ee2aa
beatrizguerra28 <83786167+beatrizguerra28@users.noreply.github.com>
2022-08-17 08:19:59 -0300

    Update ex4.hs

--- ex4.hs
+++ ex4.hs
@@ -1,9 +1,6 @@
-euclides  ::  Int  ->  Int  ->  Int 
-euclides xy 
-    | x == y = x
-    | x > y = euclídeos (x - y) y
-    |  caso contrário  = euclídeos x (y - x)
-
-main  ::  IO  ()
-main =  do
-    imprimir  $ euclídeos 6  27
+euclides  m n
+    | m == n = n
+    | otherwise = euclides(max m n - min m n) (min m n)
+    where
+        maximo = max m n
+        minimo = min m n
