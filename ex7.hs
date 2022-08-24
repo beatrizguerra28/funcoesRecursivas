c54f196a96223801f85c675387054c36ef9165e3
beatrizguerra28 <83786167+beatrizguerra28@users.noreply.github.com>
2022-08-24 08:21:23 -0300

    Update ex7.hs

--- ex7.hs
+++ ex7.hs
@@ -1,24 +1,15 @@
 
-///mergesort3 :: Ord a => [a] -> [a] -> [a]
-mergesort3 = xs [] = xs
-merge [] ys = ys
-merge (x:xs) (y:ys) = if x < y then x : merge xs (y:ys) else y : merge (x:xs) ys
+mergesort3 :: Ord a => [a] -> [a] 
+mergsort3 [] = []
+mergsort3 [x] = [x]
+mergesort3 l = merge3 (mergesort3 esq) (mergsort3 dir) 
+               where    
+                        (esq, dir) = metades3 l 
 
 metades3 l = (take n l, drop m l)
             where
-                tamanho = length l
-                m = tamanho 'div' 2
-                n = tamanho - m
-                n = if tamanho % 2 == 0 then tamanho 
-metades3 xs = splitAt (length xs `div` 2) xs
+                 tamanho = length l
+                 m = tamanho ´div´ 2
+                 n = if tamanho´mod´ 2 = 1 then tamanho - m - 1
+                 else tamanho - m
 
-mergesort :: Ord a => [a] -> [a]
-mergesort []  = []
-mergesort [x] = [x]
-mergesort xs = merge (mergesort esquerda) (mergesort direita) 
-    where (esquerda, direita) = metades xs
-
-main :: IO ()
-main = do 
-    print $ mergesort [3, 6, 8, 2, 9, 43, 75, 24, 32, 83]
-    print $ mergesort [10, 55, 87, 90, 33, 28, 74, 49, 43]
